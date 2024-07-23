ce60fca7d8f7c9ad0145cdbcbd75f5bb2e43aaf6
Marius Meyer <mariusmeyer42@gmail.com>
2024-07-23 21:10:38 +0200

    adds server object and script

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    public string Name;
+    public int Integrity;
+    public int CPU;
+    public int MemoryUnits;
+}
